deb204c24c832c81e5d77260526dea3f3bf2c27d
Ryu <goskn88006@yahoo.co.jp>
2022-02-17 15:28:14 +0900

    system　rspecテストを編集、カラムcontentにバリデーションをつける

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :task do
-    title { "MyString" }
-    content { "MyText" }
+    title { "1/2" }
+    content { "funny" }
+  end
+  factory :second_task do
+    title { "1/3" }
+    content { "funny3" }
   end
 end
