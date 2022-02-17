cea137f88b55cebd2f94fe024dc98f2aca0e97ab
Ryu <goskn88006@yahoo.co.jp>
2022-02-17 22:19:57 +0900

    Revert "Revert "system　rspecテストを編集、カラムcontentにバリデーションをつける""
    This reverts commit bdcfe169f6456274bb36fa0f6abb16dfc92d4b70.

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without a title" do
+    task = Task.new(title: nil)
+    task.valid?
+    expect(task.errors[:title]).to include("can't be blank")
+  end
+  it "is invalid without a content" do
+    task = Task.new(content: nil)
+    task.valid?
+    expect(task.errors[:content]).to include("can't be blank")
+  end
 end
