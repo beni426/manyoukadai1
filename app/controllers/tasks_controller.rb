dc6f469184a3bed097163e83f859485cf1bfdc7a
Ryu <goskn88006@yahoo.co.jp>
2022-02-19 17:48:25 +0900

    Basic認証を実装する

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
- before_action :set_task, only: %i[show  edit update destroy]
+  before_action :basic_authentication, only: :show
+  before_action :set_task, only: %i[show  edit update destroy]
   def index
     @tasks =Task.all.order(created_at: :desc)
   end
