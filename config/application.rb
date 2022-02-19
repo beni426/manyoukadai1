c65ed9e85eb842b6652d348e1a75af36e147de90
Ryu <goskn88006@yahoo.co.jp>
2022-02-19 14:35:10 +0900

    アプリケーションの国際化とタイムゾーンを設定

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,14 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module TaskManagement
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.test_framework :rspec,
                        model_specs: true,
@@ -19,10 +19,5 @@ module TaskManagement
                        controller_specs: false,
                        request_specs: false
     end
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
   end
 end
