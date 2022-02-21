0267767d9c3f289bab1b84a340fbdb3c4f77719d
Ryu <goskn88006@yahoo.co.jp>
2022-02-21 11:46:47 +0900

    ステータス（未着手・着手中・完了）が登録できるとステータスの検索

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: %i[show  edit update destroy]
   def index
     @tasks =Task.all.order(created_at: :desc)
+
     if params[:sort_expired]
       @tasks = Task.all.order(expired_at: :desc)
     end
@@ -39,7 +40,11 @@ class TasksController < ApplicationController
   end
   private
   def task_params
-    params.require(:task).permit(:title,:content, :expired_at)
+    params.require(:task).permit(
+       :title,
+       :content,
+       :expired_at,
+       :status)
   end
   def set_task
     @task = Task.find(params[:id])
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,6 @@ class Task < ApplicationRecord
     validates :title, length: { in: 1..40 } 
     validates :content, presence: true
     validates :expired_at, presence: true
+    enum status: { todo: 'todo', doing: 'doing', done: 'done' }
+   
 end
