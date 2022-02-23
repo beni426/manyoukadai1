0c96b293e5703f388576bbfa4f8b998c53c8cdcc
Ryu <goskn88006@yahoo.co.jp>
2022-02-23 11:44:20 +0900

    ユーザ新規登録/ログイン/ログアウト機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get '/',to:"tasks#index"
   resources  :tasks
+  resources  :users, only: %i[new create show]
+  resources :sessions, only: %i[new create destroy]
   
 end
