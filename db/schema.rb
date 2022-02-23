b2baa02024210e5cbaedc8646c23c43234cd19fa
Ryu <goskn88006@yahoo.co.jp>
2022-02-23 10:56:32 +0900

    ユーザとタスクを紐づける

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_121354) do
+ActiveRecord::Schema.define(version: 2022_02_23_015352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,10 @@ ActiveRecord::Schema.define(version: 2022_02_22_121354) do
     t.datetime "expired_at"
     t.string "status", default: "new", null: false
     t.integer "priority", default: 1, null: false
+    t.bigint "user_id", null: false
     t.index ["status"], name: "index_tasks_on_status"
     t.index ["title"], name: "index_tasks_on_title"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema.define(version: 2022_02_22_121354) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
